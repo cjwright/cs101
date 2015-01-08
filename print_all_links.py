6074c20cbbe0fa8db9685d168c76432f724e9229
Callum Wright <callumjwright@gmail.com>
2015-01-08 15:58:05 +0000

    Added get_next_target(s) function

--- print_all_links.py
+++ print_all_links.py
@@ -5,4 +5,13 @@ def get_page(url):
     except:
         return ""
 
+def get_next_target(s):
+    start_link = s.find('<a href=')
+    if start_link == -1:
+       return None,0
+    else:
+        start_quote = s.find('"', start_link+1)
+        end_quote = s.find('"',start_quote+1)
+        url = s[start_quote+1:end_quote]
+    return url, end_quote
 
